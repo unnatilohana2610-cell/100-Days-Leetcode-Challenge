3764c82cb7cacf98db4c9bcd29cf521999d4d41d
unnatilohana2610-cell <unnatilohana2610@gmail.com>
2026-02-03 21:07:25 +0530

    day1 code added

---
+++ Day1/solution.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            minPrice = min(minPrice, price);
+            maxProfit = max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+};
